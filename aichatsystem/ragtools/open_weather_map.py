31466f7dac91545e2328f6a6e2c5b7ab0ed9c29f
KIKUKOU <baseballsoul.no3@gmail.com>
2024-10-03 13:54:25 +0900

    typo wether to weather

--- aichatsystem/ragtools/open_weather_map.py
+++ aichatsystem/ragtools/open_weather_map.py
@@ -1,24 +1,22 @@
 #!/usr/bin/env python3
 """
-The class for open werher map(get wether).
+The class for open werher map(get weather).
 """
 
 from __future__ import annotations
 
-import datetime
+from datetime import datetime, timedelta
 from typing import Any
 
+import pytz
 import requests
 
 from .rag_tools_template import RagTools
 
-api_key = 'a51b01c289dfcc6c5825e18dec77592c'
-city = 'Tokyo'
 
-
-class OpenWetherMap(RagTools):
+class OpenWeatherMap(RagTools):
     """
-    Search the wether.
+    Search the weather.
     """
 
     ERROR_STATUS_CODE = 200
@@ -32,8 +30,11 @@ class OpenWetherMap(RagTools):
         Initialize the RAG tools.
 
         Args:
-            key (str): The API key for OpenAI.
+            key (str): The API key for open weather map.
             config (dict[str, Any] | None, optional): Configuration options for the tool. Defaults to None.
+
+        Raises:
+            ValueError: If the API key is invalid.
         """
         try:
             self.api_key = key
@@ -57,10 +58,9 @@ class OpenWetherMap(RagTools):
                 :: (S) -90 < latitude < 90 (N), (W) -180 < longitude < 180 (E).
                 :: time(h) minus is before.
             config (dict[str, Any], optional): Configuration for this specific request. Defaults to None.
-            is_streaming (bool, optional): Whether to stream the response. Defaults to False.
 
         Returns:
-            Any: The response from the LLM.
+            Any: The response from open weather map 5days(3h) weather.
         """
         url = 'https://api.openweathermap.org/data/2.5/forecast'
         self._validate_keywords(keywords)
@@ -74,23 +74,14 @@ class OpenWetherMap(RagTools):
         if response.status_code != self.ERROR_STATUS_CODE:
             raise (f"error: {data['message']}")
 
-        wether_time = datetime.datetime.today() + datetime.timedelta(hours=keywords['time'])  # noqa: DTZ002
-        # TODO: Set timezone. #  noqa: FIX002
-        # ISSUE-001
-        wether_time = wether_time.replace(microsecond=0)
-        for forecast in data['list']:
-            forecast_time = datetime.datetime.fromtimestamp(forecast['dt'])  # noqa: DTZ006
-            if forecast_time >= wether_time:
-                return forecast
-
-        return None
+        return data
 
     def _search_by_city(self, url: str, keywords: dict[str, Any]) -> str:
         """
         Get the url by city name.
 
         Args:
-            url (str): The wether api url.
+            url (str): The weather api url.
             keywords (dict[str, Any]): The input keywards.
 
         Returns:
@@ -106,7 +97,6 @@ class OpenWetherMap(RagTools):
                 'q': city,
                 'appid': self.api_key,
                 'units': 'metric',
-                'lang': 'ja',
             },
             timeout=10,
         )
@@ -116,7 +106,7 @@ class OpenWetherMap(RagTools):
         Get the url by city name.
 
         Args:
-            url (str): The wether api url.
+            url (str): The weather api url.
             keywords (dict[str, Any]): The input keywards.
 
         Returns:
@@ -134,7 +124,6 @@ class OpenWetherMap(RagTools):
                 'lon': lon,
                 'appid': self.api_key,
                 'units': 'metric',
-                'lang': 'ja',
             },
             timeout=10,
         )
@@ -156,3 +145,70 @@ class OpenWetherMap(RagTools):
         if 'city' not in keywords and ('lat' not in keywords or 'lon' not in keywords):
             raise_message = 'Invalid keywords in forecast point. It need time and city or time and lat and lon'
             raise ValueError(raise_message)
+
+    @staticmethod
+    def convert_weather_data_by_5days(data: dict[str, Any]) -> dict[str, Any]:
+        """
+        convert data for ai input.
+
+        Args:
+            data (dict[str, Any]): The raw data from open weather map by 5days forcust.
+
+        Raises:
+            ValueError: If the keywords is invalid.
+        """
+        output = {
+            'list': [],
+            'city': {
+                'name': data['city']['name'],
+                'coord': data['city']['coord'],
+                'country': data['city']['country'],
+                'timezone': data['city']['timezone'],
+                'sunrise': data['city']['sunrise'],
+                'sunset': data['city']['sunset'],
+            },
+        }
+
+        jst = pytz.timezone('Asia/Tokyo')
+        now = datetime.now(jst)
+        today = now.date()
+        tomorrow = today + timedelta(days=1)
+        day_after_tomorrow = today + timedelta(days=2)
+
+        for item in data['list']:
+            dt = datetime.fromtimestamp(item['dt'], pytz.UTC).astimezone(jst)
+            item_date = dt.date()
+
+            # 今日と明日の条件
+            condition_today_tomorrow = (
+                item_date <= tomorrow and (dt.hour in [0, 6, 12, 18] or dt <= now) and dt.minute == 0
+            )
+
+            # 明後日以降の条件
+            condition_future = item_date >= day_after_tomorrow and dt.hour in [12] and dt.minute == 0
+
+            if condition_today_tomorrow or condition_future:
+                new_item = {
+                    'dt': item['dt'],
+                    'main': {
+                        'temp': item['main']['temp'],
+                        'feels_like': item['main']['feels_like'],
+                        'pressure': item['main']['pressure'],
+                        'humidity': item['main']['humidity'],
+                    },
+                    'weather': [
+                        {'main': item['weather'][0]['main'], 'description': item['weather'][0]['description']},
+                    ],
+                    'clouds': item['clouds'],
+                    'wind': item['wind'],
+                    'pop': item['pop'],
+                    'dt_txt': dt.strftime('%Y-%m-%d %H:%M:%S'),
+                }
+
+                if 'rain' in item:
+                    new_item['rain'] = item['rain']
+
+                output['list'].append(new_item)
+
+        output['list'].sort(key=lambda x: x['dt'])
+        return output
